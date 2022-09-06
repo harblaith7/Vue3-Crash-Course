af70b5f345fd17b2e08fb863d490873c908049ea
Laith Harb <laithharb@Laiths-iMac.local>
2022-09-06 08:08:02 -0400

    yes

--- 03-Quiz/src/views/QuizView.vue
+++ 03-Quiz/src/views/QuizView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import quizes from "../data/quiz.json"
-import {ref, onBeforeMount} from "vue"
-import {useRoute} from "vue-router"
+import {ref, onBeforeMount, computed} from "vue"
+import {useRoute, RouterLink} from "vue-router"
 import Question from "../components/Question.vue"
 
 const route = useRoute();
@@ -15,13 +15,29 @@ onBeforeMount(() => {
     quiz.value = quizes.find(q => q.id === parseInt(route.params.id))
 })
 
+const status = computed(() => {
+    if(quiz.value){
+        return `${currentQuestion.value}/${quiz.value.questions.length}`
+    }
+})
+
+const barPercentage = computed(() => {
+    if(quiz.value){
+        if(currentQuestion.value === 0) return "0%";
+        else {
+            console.log((currentQuestion.value)/quiz.value.questions.length * 100)
+            return `${currentQuestion.value/quiz.value.questions.length * 100}%`
+        }
+    } 
+})
+
 const onOptionSelected = (isCorrect) => {
     if(isCorrect){
         numberOfCorrectAnswers.value++;
     };
 
     if(quiz.value.questions.length -1 === currentQuestion.value){
-        return showResults.value = true
+        showResults.value = true
     }
     currentQuestion.value++
 }
@@ -30,14 +46,25 @@ const onOptionSelected = (isCorrect) => {
 <template>
     <div>
         <header>
-            <div class="bar"></div>
+            <h4>Question {{status}}</h4>
+            <div class="bar">
+                <div class="completion" :style="{width: barPercentage}"></div>
+            </div>
         </header>
         <transition name="bounce" mode="out-in">
             <Question v-if="!showResults"
                 :question="quiz.questions[currentQuestion]"
                 @selectOption="onOptionSelected"
             />
-            <div v-else>done</div>
+            <div v-else>
+                <div class="results">
+                    <p>Your Results...</p>
+                    <h1>
+                        {{numberOfCorrectAnswers}}/{{quiz.questions.length}}
+                    </h1>
+                    <RouterLink to="/">Go Back</RouterLink>
+                </div>
+            </div>
         </transition>
     </div>
 </template>
@@ -55,11 +82,48 @@ const onOptionSelected = (isCorrect) => {
         }
     }
 
+    .bar {
+        width: 300px;
+        height: 50px;
+        border: 3px solid bisque;
+    }
+
+    .completion {
+        background-color: bisque;
+        height: 100%;
+    }
+
     .bounce-enter-active {
         animation: bounce-in 0.5s;
     }
 
     .bounce-leave-active {
         animation: bounce-in 0.5s reverse;
+    }
+
+    header {
+        margin-top: 20px;
+    }
+
+    h4 {
+        font-size: 30px;
+    }
+
+    .results {
+        text-align: center;
+        padding: 100px 0
+    }
+
+    .results h1 {
+        font-size: 100px;
+    }
+
+    .results p {
+        font-size: 25px;
+    }
+
+    .results button {
+        padding: 10px 30px;
+        font-size: 20px;
     }
 </style>
